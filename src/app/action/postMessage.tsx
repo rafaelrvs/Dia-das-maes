fac7f7940a7dd66301c39f5b0ac8bd044c7c9ef2
rafaelrvs <rafaelvieira1333@gmail.com>
2025-05-09 23:06:19 -0300

    adcs

--- src/app/action/postMessage.tsx
+++ src/app/action/postMessage.tsx
@@ -82,7 +82,7 @@ export async function generateMessageAction({
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.NEXT_PUBLIC_GEMINI_API_KEY}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
