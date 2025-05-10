12772cf784bebcc3fb510b04470d0017bb580a13
rafaelrvs <rafaelvieira1333@gmail.com>
2025-05-09 22:35:49 -0300

    adc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...suas outras configurações
-
   // 1. não travar o build por erros de ESLint
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,10 +7,14 @@ const nextConfig = {
 
   // 2. não travar o build por erros de TypeScript
   typescript: {
-    
     ignoreBuildErrors: true,
   },
 
+  // 3. gerar source maps no build de produção
+  productionBrowserSourceMaps: true,
+
+  // Opcional: desative a minificação se quiser bundles legíveis
+  // swcMinify: false,
 };
 
 module.exports = nextConfig;
